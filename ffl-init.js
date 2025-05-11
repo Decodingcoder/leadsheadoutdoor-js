4e0c828203f3d612a3edf02dd744003abab6f916
Decodingcoder <134673860+Decodingcoder@users.noreply.github.com>
2025-05-12 00:05:42 +0100

    ffl-init.js

--- ffl-init.js
+++ ffl-init.js
@@ -1,14 +1,13 @@
 // ffl-init.js
-
-// Initialize the FFL widget when the SDK is ready
-FFL.init({
-  container: 'ffl_container',
-  apiKey: 'V5ThVSNeQHwXMLESio6ed7HE9JwyEacfRe7TtOJR458y0GnhsZU7xMwkhyvKKRRz6814c201e7b07',
-  geoKey: 'AIzaSyBeWDDQUtesS9aAYT7Y4BSw_MCv6ItnS4k',
-  cBack: getSelected
+document.addEventListener('DOMContentLoaded', function() {
+  FFL.init({
+    container: 'ffl_container',
+    apiKey:    'V5ThVSNeQHwXMLESio6ed7HE9JwyEacfRe7TtOJR458y0GnhsZU7xMwkhyvKKRRz6814c201e7b07',
+    geoKey:    'AIzaSyBeWDDQUtesS9aAYT7Y4BSw_MCv6ItnS4k',
+    cBack:     getSelected
+  });
 });
 
 function getSelected(data) {
-  // handle selected data here
   console.log('FFL callback data:', data);
 }
