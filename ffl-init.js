6da2e44c5ebdbbcc81dd264d9bec25664ac321de
Decodingcoder <134673860+Decodingcoder@users.noreply.github.com>
2025-05-12 01:08:24 +0100

    ffl-init.js

--- ffl-init.js
+++ ffl-init.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
+// Define initGMap so Maps is loaded only once, then init the widget
+FFL.initGMap = function() {
   FFL.init({
     container: 'ffl_container',
-    apiKey: 'YOUR_API_KEY',
-    geoKey: 'YOUR_MAPS_KEY',
-    cBack: (data) => console.log('chosen dealer:', data)
+    apiKey:    'V5ThVSNeQHwXMLESio6ed7HE9JwyEacfRe7TtOJR458y0GnhsZU7xMwkhyvKKRRz6814c201e7b07',
+    geoKey:    'AIzaSyBeWDDQUtesS9aAYT7Y4BSw_MCv6ItnS4k',
+    cBack:     data => console.log('chosen dealer:', data)
   });
-});
+};
